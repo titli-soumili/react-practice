512f455411361ba07497fe515d29d3caf904cb8e
Soumili <soumilichakraborty7@gmail.com>
2022-01-04 21:05:13 +0530

    Destructuring of Props and States

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/setState/Counter';
+import Greet from './destructuringProps/Greet';
+
+
+class App extends Component {  
+  render() {
+    return (
+      <div className="App">
+        {/* <Counter/> */}
+        <Greet name="Danial" heroName="Harry"/>
+      </div>
+    )
+  }
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
