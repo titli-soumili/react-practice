2472a21b5b57ea1a359803894e8947afe1e760be
Soumili Chakraborty <soumilichakraborty7@gmail.com>
2022-03-16 19:16:22 +0530

    modified code

--- ES6 Javascript/const_let.js
+++ ES6 Javascript/const_let.js
@@ -21,18 +21,18 @@ fName = "soumili";     // reassigned allowed
 console.log('name', fName);
 
 
-// Using "let", a variable can be reassigned but can not be redefined
-let fName = "soumili";
-let fName = "kunal";  // redefined not allowed
-fName = "soumili";     // reassigned allowed
-console.log('name', fName);
+// // Using "let", a variable can be reassigned but can not be redefined
+// let fName = "soumili";
+// let fName = "kunal";  // redefined not allowed
+// fName = "soumili";     // reassigned allowed
+// console.log('name', fName);
 
 
-//?? Using "const", a variable can neither be redefined nor be reassigned
-const fName = "soumili";
-const fName = "kunal";  // redefined not allowed
-fName = "soumili";     // reassigned not allowed
-console.log('name', fName);
+// //?? Using "const", a variable can neither be redefined nor be reassigned
+// const fName = "soumili";
+// const fName = "kunal";  // redefined not allowed
+// fName = "soumili";     // reassigned not allowed
+// console.log('name', fName);
 
 
 // Block scoping
@@ -46,21 +46,21 @@ if(fullname) {
 console.log(firstname); // allowed
 
 
-// Variable defined using "let" can not be accessable out of the scope
-var fullname = 'Soumili Chakraborty';
-let firstname;
-if(fullname) {
-    firstname = fullname.split(' ')[0];
-    console.log(firstname);
-}
-console.log(firstname); // not allowed
+// // Variable defined using "let" can not be accessable out of the scope
+// var fullname = 'Soumili Chakraborty';
+// let firstname;
+// if(fullname) {
+//     firstname = fullname.split(' ')[0];
+//     console.log(firstname);
+// }
+// console.log(firstname); // not allowed
 
 
-//?? Variable defined using "const" can not be accessable out of the scope
-const fullname = 'Soumili Chakraborty';
-const firstname;
-if(fullname) {
-    firstname = fullname.split(' ')[0];
-    console.log(firstname);
-}
-console.log(firstname); // not allowed
+// //?? Variable defined using "const" can not be accessable out of the scope
+// const fullname = 'Soumili Chakraborty';
+// const firstname;
+// if(fullname) {
+//     firstname = fullname.split(' ')[0];
+//     console.log(firstname);
+// }
+// console.log(firstname); // not allowed
